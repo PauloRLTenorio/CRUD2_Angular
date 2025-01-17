1ea0a242fb25dc010961b3ffb6966977838c9b0c
Paulo Tenório <paulorltenorio@gmail.com>
2025-01-17 16:02:05 -0300

    Atualizado o arquivo list-items.component.ts com a lógica de exibição dos itens.

--- src/app/list-items/list-items.component.ts
+++ src/app/list-items/list-items.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ItemService } from '../services/item.service';
 
 @Component({
   selector: 'app-list-items',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './list-items.component.html',
-  styleUrl: './list-items.component.css'
+  styleUrls: ['./list-items.component.css'],
 })
-export class ListItemsComponent {
+export class ListItemsComponent implements OnInit {
+  title = 'CRUD_Angular';
+  items: any[] = [];
 
+  constructor(private readonly itemService: ItemService) {}
+
+  ngOnInit() {
+    this.loadItems();
+  }
+
+  loadItems() {
+    this.itemService.getAllItems().subscribe({
+      next: (data) => {
+        this.items = data;
+      },
+      error: (error) => {
+        console.error('Erro ao carregar os itens:', error);
+      },
+    });
+  }
 }
