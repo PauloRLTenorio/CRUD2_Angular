1ac2249b3003cabfb22372b25be9f1840bff70f3
Paulo Tenório <paulorltenorio@gmail.com>
2025-01-20 12:29:24 -0300

    Configuração inicial das rotas para listagem e criação de itens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListItemsComponent } from './list-items/list-items.component';
+import { CreateItemComponent } from './create-item/create-item.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' }, // Redireciona a raiz para a rota 'list'
+  { path: 'list', component: ListItemsComponent }, // Rota para listagem
+  { path: 'create', component: CreateItemComponent }, // Rota para criar item
+  { path: '**', redirectTo: 'list' }, // Redireciona rotas inválidas para 'list'
+];
