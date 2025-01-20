e16759b40b47fa4fd46da119f60e04bd61158da9
Paulo Tenório <paulorltenorio@gmail.com>
2025-01-20 12:38:24 -0300

    feat(list-items): implementada funcionalidade de pesquisa e integração com serviço de itens. (ts) e  feat(list-items): adicionada funcionalidade de pesquisa e renderização condicional de itens. (html)

--- src/app/list-items/list-items.component.ts
+++ src/app/list-items/list-items.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ItemService } from '../services/item.service';
+import { SearchItemsComponent } from '../search-items/search-items.component';
 
 @Component({
   selector: 'app-list-items',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SearchItemsComponent],
   templateUrl: './list-items.component.html',
   styleUrls: ['./list-items.component.css'],
 })
 export class ListItemsComponent implements OnInit {
   title = 'CRUD_Angular';
   items: any[] = [];
+  searchedItems: any[] = [];
+  searchExecuted: boolean = false;
+  isSearchActive: boolean = false; // Variável de controle
+
 
   constructor(private readonly itemService: ItemService) {}
 
@@ -23,11 +28,26 @@ export class ListItemsComponent implements OnInit {
   loadItems() {
     this.itemService.getAllItems().subscribe({
       next: (data) => {
-        this.items = data;
+        this.items = data; // Carrega todos os itens ao carregar a rota
       },
       error: (error) => {
         console.error('Erro ao carregar os itens:', error);
       },
     });
   }
+
+
+  handleSearch(query: string): void {
+    this.searchedItems = this.items.filter((item) => {
+      const lowerQuery = query.toLowerCase();
+      return (
+        item.name.toLowerCase().includes(lowerQuery) ||
+        item.description.toLowerCase().includes(lowerQuery) ||
+        item.price.toString().includes(lowerQuery)
+      );
+    });
+    this.searchExecuted = true;
+  }
+
+
 }
