31c85a542b0ab0c5bce12c573e2e91a0210e69b7
Paulo Tenório <paulorltenorio@gmail.com>
2025-01-25 04:00:55 -0300

    Criado componente para botão editar item

---
+++ src/app/edit-item-button/edit-item-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-item-button',
+  imports: [],
+  templateUrl: './edit-item-button.component.html',
+  styleUrl: './edit-item-button.component.css'
+})
+export class EditItemButtonComponent {
+
+}
