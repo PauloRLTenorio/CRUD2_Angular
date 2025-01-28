adf00643599c1d6cf2f0f55c965dea0ba6955bee
Paulo Tenório <paulorltenorio@gmail.com>
2025-01-28 02:04:22 -0300

    importando EditItemButtonComponent

--- src/app/found-items/found-items.component.ts
+++ src/app/found-items/found-items.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DeleteItemComponent } from '../delete-item/delete-item.component';
+import { EditItemButtonComponent } from "../edit-item-button/edit-item-button.component";
 
 @Component({
   selector: 'app-found-items',
   standalone: true,
-  imports: [CommonModule, RouterModule, DeleteItemComponent],
+  imports: [CommonModule, RouterModule, DeleteItemComponent, EditItemButtonComponent],
   templateUrl: './found-items.component.html',
   styleUrls: ['./found-items.component.css'],
 })
