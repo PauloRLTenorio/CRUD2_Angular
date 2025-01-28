16d796ee0917498ed894ed20872909764374f67f
Paulo Tenório <paulorltenorio@gmail.com>
2025-01-28 02:12:33 -0300

    Refatoração: adicionados Input e navegação no componente ‘edit-item-button’, permitindo navegação para a rota de edição com o ID do item.

--- src/app/edit-item-button/edit-item-button.component.ts
+++ src/app/edit-item-button/edit-item-button.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-item-button',
-  imports: [],
+  standalone: true,
   templateUrl: './edit-item-button.component.html',
-  styleUrl: './edit-item-button.component.css'
+  styleUrls: ['./edit-item-button.component.css'],
 })
 export class EditItemButtonComponent {
+  @Input() itemId!: number; // Recebe o ID do item como entrada
 
+  constructor(private readonly router: Router) {}
+
+  navigateToEdit() {
+    this.router.navigate(['/edit', this.itemId]); // Navega para a rota de edição
+  }
 }
